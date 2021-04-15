8c0c0e1b53bc35a4bb87fb817ca15747849065c1
DavJosBer <boletindavid1995@gmail.com>
2021-04-15 04:34:32 +0000

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    address = db.Column(db.String(250)) 
+    client = db.relationship('Ordenes', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
-            "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "address": self.address
+        }
+    def check_password(self, password):
+        return safe_str_cmp(password, self.password)
+
+class Ordenes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+    def __repr__(self):
+        return '<Service %s>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(250), unique=True)
+    costo = db.Column(db.Integer)
+    client = db.relationship('Ordenes', backref='service', lazy=True)
+
+    def __repr__(self):
+        return '<Service %s>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,84 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import re
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Ordenes, Service
 from api.utils import generate_sitemap, APIException
+from datetime import timedelta
+from flask_jwt_extended import current_user
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
+@api.route('/', methods=['GET'])
+def hello():
+    
     response_body = {
         "message": "Hello! I'm a message that came from the backend"
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    
+    body = request.get_json()
+    user = User()
+    
+    if 'username' not in body:
+        return "username required",400
+    if 'email' not in body:
+        return "email required",400
+    if 'password' not in body:
+        return 'password required',400
+    if not re.match('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,8}$', body['email']):
+        return 'enter a valid format check your email'
+    if not re.match('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W])[^\n\t]{8,20}$', body['password']):
+        return 'Password must contain the following: a lowercase letter, a capital letter, a number, one special character and minimum 8 characters'
+
+    username = User.query.filter_by(username=body['username']).first()
+    email = User.query.filter_by(email=body['email']).first()
+
+    if username:
+        return 'This username already exists. Check your username'
+    if email:
+        return 'This email already exists. Check your email'
+    
+    user.username = body['username']
+    user.email = body['email']
+    user.password = body['password']
+    user.address = body['address']
+
+    db.session.add(user)
+    db.session.commit()
+
+    response_body = {
+        'msg': "user commited"
+    }
+
+    return jsonify(response_body),200
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username is None:
+        return jsonify({"msg": "Username is required"}), 400
+    if password is None:
+        return jsonify({"msg": "Password is required"}),401
+    
+    user = User.query.filter_by(username=username).one_or_none()
+
+    if not user:
+        return jsonify({"msg": "Username doesn't exist"}), 400
+    if not user.check_password(password):
+        return jsonify({"msg": "Invalid password"}), 401
+    
+    expiration = timedelta(days=1)
+    access_token = create_access_token(identity=user, expires_delta=expiration)
+    return jsonify('The login has been successful.', {'token':access_token}), 200
